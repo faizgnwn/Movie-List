2500bb46da53d865401be993646ccd56bd329682
faizgnwn <111131554+faizgnwn@users.noreply.github.com>
2022-10-17 20:53:46 +0700

    Update data-source.js

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -2,7 +2,8 @@ import movie from './movies.js';
 
 class DataSource {
   static searchMovie(keyword) {
-    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=bee359088ec56031386d27f4a16a8205&language=en-US&page=1&include_adult=false&query=${keyword}`)
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=bee359088ec56031386d27f4a16a8205&language=en-US&query=%24%7Bkeyword%7D&page=1&include_adult=false`)
+
       .then(response => {
         return response.json();
       })
