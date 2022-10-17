3257e56755c706f52b329836671dcbd6c617fab7
faizgnwn <111131554+faizgnwn@users.noreply.github.com>
2022-10-17 23:34:27 +0700

    Update data-source.js

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -2,13 +2,13 @@ import movie from './movies.js';
 
 class DataSource {
   static searchMovie(keyword) {
-    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=bee359088ec56031386d27f4a16a8205&language=en-US&query=%24%7Bkeyword%7D&page=1&include_adult=false`)
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=bee359088ec56031386d27f4a16a8205&language=en-US&page=1&include_adult=false&query=${keyword}`)
 
       .then(response => {
         return response.json();
       })
       .then(responseJson => {
-        if (responseJson.results) {
+        if (responseJson.results.length !==0) {
           return Promise.resolve(responseJson.results);
         } else {
           return Promise.reject(`${keyword} is not found`);
